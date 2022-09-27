1989ae241880d9d716d4337e002a4a7c8fd382d4
Wei Lun <garyganweilun@gmail.com>
2022-09-27 18:00:24 +0800

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useForm, useFieldArray } from "react-hook-form";
+import { toBech32Address } from "@zilliqa-js/crypto"
+import { useState } from 'react';
+
 
 function App() {
+  const [toBench32, settoBench32] = useState([])
+  const { register, control, handleSubmit } = useForm({
+
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "test"
+  });
+
+  const handledata = (data) => {
+    data.test.map((data) => {
+
+      settoBench32( arr => [... arr, (toBech32Address(data.bench16))])
+    })
+
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <form onSubmit={handleSubmit(handledata)}>
+        <ul>
+          {fields.map((item, index) => (
+            <li key={item.id}>
+              <input {...register(`test.${index}.bench16`)} />
+              <button type="button" onClick={() => remove(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={() => append({})}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          append
+        </button>
+        <input type="submit" />
+      </form>
+     {toBench32.map((data)=>
+      {
+        return(<p>{data}</p>)
+      })}
+    </>
   );
 }
 
